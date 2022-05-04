0d93cedaa4cbb1e9225d732033b444ece1e45746
unxoun <unxoun@gmail.com>
2022-05-04 02:29:52 -0800

    refactor: yey.

--- components/component.jsx
+++ components/component.jsx
@@ -9,13 +9,13 @@ export default function ReduxToolKit() {
   //
   const color = useSelector((state) => state.color);
   const handleRedColorButtonClick = () => {
-    dispatch(changedToRed('I am a useless payload value!'));
+    dispatch(changedToRed("I am a useless payload value!"));
   };
 
   //
   const count = useSelector((state) => state.count);
   const handleIncrementBy1ButtonClick = () => {
-      dispatch(incrementedBy1('I am a useless payload value!'))
+    dispatch(incrementedBy1("I am a useless payload value!"));
   };
 
   //
@@ -24,13 +24,12 @@ export default function ReduxToolKit() {
       <button name="redColorButton" onClick={handleRedColorButtonClick}>
         Red
       </button>
-      <button name="blueColorButton">Blue</button>
       <button name="greenColorButton">Green</button>
-      <div style={{ background: `${color}` }}>state.color</div>
+      <div>state.count: {color}</div>
+
       <button onClick={handleIncrementBy1ButtonClick}>+1</button>
-      <button>+7</button>
-      <button>+58</button>
-      <div style={{ background: "#ccc" }}>state.count: {count}</div>
+      <button>+10</button>
+      <div>state.count: {count}</div>
     </>
   );
 }
