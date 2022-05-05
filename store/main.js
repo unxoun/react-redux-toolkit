faa5d4c56b6946cf400f6b9d58604c7c58c53c99
unxoun <60313778+unxoun@users.noreply.github.com>
2022-05-05 04:35:41 -0800

    Update main.js

--- store/main.js
+++ store/main.js
@@ -1,12 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+
+// Reducers
 import color from "./slices/color";
 import count from "./slices/count";
+
+// Middleware
+import middleware1 from "./middleware/middleware-1";
+import middleware2 from "./middleware/middleware-2";
 
 export default configureStore({
   reducer: {
     color,
     count,
   },
-          middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(middleware1, middleware2, middleware3),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middleware1, middleware2),
 });
